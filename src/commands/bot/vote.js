1c65d2189060ab3364881bf307d30eef360192f2
Horizon Development <111465344+testdrake@users.noreply.github.com>
2024-04-30 19:54:25 -0700

    Update vote.js

--- src/commands/bot/vote.js
+++ src/commands/bot/vote.js
@@ -1,41 +1,17 @@
 const Discord = require('discord.js');
-const Topgg = require(`@top-gg/sdk`);
-const moment = require("moment");
-require("moment-duration-format");
 
 module.exports = async (client, interaction, args) => {
-    let dbl = new Topgg.Api(process.env.TOPGG_TOKEN)
-
-    let row = new Discord.ActionRowBuilder()
-        .addComponents(
-            new Discord.ButtonBuilder()
-                .setLabel("Vote for me")
-                .setURL("https://top.gg/bot/798144456528363550/vote")
-                .setStyle(Discord.ButtonStyle.Link),
-        );
-
-    dbl.hasVoted(interaction.user.id).then(voted => {
-        if (voted) {
-            client.embed({
-                title: `📨・Vote`,
-                desc: `You have voted!`,
-                image: `https://cdn.discordapp.com/attachments/843487478881976381/874694192755007509/Bot_banner_vote.jpg`,
-                color: client.config.colors.succes,
-                components: [row],
-                type: 'editreply'
-            }, interaction)
-        }
-        if (!voted) {
-            client.embed({
-                title: `📨・Vote`,
-                desc: `You have not voted!`,
-                image: `https://cdn.discordapp.com/attachments/843487478881976381/874694192755007509/Bot_banner_vote.jpg`,
-                color: client.config.colors.error,
-                components: [row],
-                type: 'editreply'
-            }, interaction)
-        }
-    }).catch(error => { client.errNormal({ text: `There was an error by checking this vote!`, editreply: true }, interaction) });
+    client.embed({
+        title: `📨・Upvote the Bot`,
+        desc: `**You can upvote the bot on the following platforms:**
+- [botlist.me](https://botlist.me/bots/1217203597603897535/vote)
+- [infinitybots.gg](https://infinitybots.gg/bot/1217203597603897535/vote)
+- [discordlist.gg](https://discordlist.gg/bot/1217203597603897535/vote)
+- [discordbotlist.com](https://discordbotlist.com/bots/horizon-8253/upvote)
+- [bhlist.co.in](https://bhlist.co.in/bots/1217203597603897535/vote)
+- [topbotlist.xyz](https://topbotlist.xyz/bot/1217203597603897535/vote)`,
+        thumbnail: client.user.avatarURL({ dynamic: true, size: 1024 }),
+        type: 'editreply'
+    }, interaction)
 }
-
  
